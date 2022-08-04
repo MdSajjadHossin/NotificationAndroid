94a68efcf53283711c66152003b067d186964779
MdSajjadHossin <iamsajjadhossin@gmail.com>
2022-08-04 15:02:37 +0600

    custom notification added

--- app/src/main/java/com/example/notificationandroid/MainActivity.java
+++ app/src/main/java/com/example/notificationandroid/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.core.content.res.ResourcesCompat;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -13,8 +15,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     public static  final  String CHANNEL_ID = "Message Channel";
-
     public static final int NOTIFICATION_ID = 100;
+    public static final int REQ_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,36 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         Notification notification;
+        Intent inotify = new Intent(getApplicationContext(), MainActivity.class);
+        inotify.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pI = PendingIntent.getActivity(this, REQ_CODE, inotify,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //Big Picture Style
+        Notification.BigPictureStyle bigPictureStyle = new Notification.BigPictureStyle()
+                .bigPicture(((BitmapDrawable) (ResourcesCompat.getDrawable(getResources(),R.drawable.system,null))).getBitmap())
+                .bigLargeIcon(largeIcon)
+                .setBigContentTitle("Image Sent By Sajid")
+                .setSummaryText("Media Message");
+
+
+        //Inbox Style
+        Notification.InboxStyle inboxStyle = new Notification.InboxStyle()
+                .addLine("A")
+                .addLine("B")
+                .addLine("C")
+                .addLine("D")
+                .addLine("E")
+                .addLine("F")
+                .addLine("G")
+                .addLine("H")
+                .addLine("I")
+                .addLine("J")
+                .addLine("K")
+                .addLine("L")
+                .setBigContentTitle("Complete Message")
+                .setSummaryText("Message From Sajid");
+
 
         //Creating Notification
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
@@ -35,7 +67,10 @@ public class MainActivity extends AppCompatActivity {
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.system)
                     .setContentText("New Message")
-                    .setSubText("New Message From Paglu")
+                    .setSubText("New Message From Rahul")
+                    .setContentIntent(pI)
+                    .setStyle(inboxStyle)
+                    .setAutoCancel(false)
                     .setChannelId(CHANNEL_ID)
                     .build();
             notificationManager.createNotificationChannel(new NotificationChannel(CHANNEL_ID, "New Channel", NotificationManager.IMPORTANCE_HIGH));
@@ -44,7 +79,10 @@ public class MainActivity extends AppCompatActivity {
                     .setLargeIcon(largeIcon)
                     .setSmallIcon(R.drawable.system)
                     .setContentText("New Message")
-                    .setSubText("New Message From Paglu")
+                    .setSubText("New Message From Paplu")
+                    .setContentIntent(pI)
+                    .setAutoCancel(false)
+                    .setStyle(inboxStyle)
                     .build();
 
         }
